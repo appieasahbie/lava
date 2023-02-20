77965e85322e1b162dd962c97b0d3bd3853a43a2
Appieasahbie | AkNodes <108979536+appieasahbie@users.noreply.github.com>
2023-02-20 14:40:54 +0100

    Update lava.sh

--- lava.sh
+++ lava.sh
@@ -149,13 +149,10 @@ echo -e "      \e[1m\e[32m!!!!!!!!!SAVE!!!!!!!!!!!!!!!!SAVE YOUR MNEMONIC PHRASE
 break
 ;;
 "Synchronization via Snapshot")
- sudo systemctl stop lavad
  cp $HOME/.lava/data/priv_validator_state.json $HOME/.lava/priv_validator_state.json.backup 
  lavad tendermint unsafe-reset-all --home $HOME/.lava --keep-addr-book 
  curl https://snapshot.lava.aknodes.net/snapshot-lava-02-20.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.lava
  mv $HOME/.lava/priv_validator_state.json.backup $HOME/.lava/data/priv_validator_state.json 
- sudo systemctl start lavad
- sudo journalctl -u lavad -f --no-hostname -o cat
 
 break
 ;;
